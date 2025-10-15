fa5f86a0211d23ef30f05eccbe023147a52df9d4
S3th5043 <sethrupasinghe@gmail.com>
2025-10-15 19:40:47 +1100

    Revert "feat: add Next.js 14 app with Tailwind, auth, dashboards, creation fl…"
    This reverts commit d48da55490a17040aab9f95f6bcf568406211eec.

--- src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export type Testimonial = { name: string; text: string };
-export type Plan = { name: string; price: string; features: string[] };
--- src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export const cx = (...classes: Array<string | undefined | null | false>) =>
-  classes.filter(Boolean).join(' ');
